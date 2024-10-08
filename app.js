0d05d17d6dd9a22c5d7f941c9acd4d688ba4c461
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-10-08 11:17:54 +0700

    Add Sequelize authentication to check database connection

--- app.js
+++ app.js
@@ -46,5 +46,9 @@ app.use('/auth', authRoutes);
 app.use('/api', categoriesRoutes);
 app.use('/api/', uploadRoutes);
 
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
