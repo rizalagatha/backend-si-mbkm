d2cd351e8cec9ec079daa0b58393f6d20908f9f9
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-10-10 11:45:37 +0700

    Update database configuration and migrations

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config(); // Mengambil environment variables dari .env
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
