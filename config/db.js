1f3e88b2b436532222d76be2c406c45cca9387b6
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-10-10 10:36:50 +0700

    Update SSL configuration for database connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const pg = require('pg');
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 
@@ -10,9 +12,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
-  }
+      rejectUnauthorized: false,
+      ca: fs.readFileSync(path.join(__dirname, 'cert', 'prod-ca-2021.crt')).toString(),
+    },
+  },
 });
 
 sequelize.authenticate()
