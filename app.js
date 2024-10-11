ee97b84fc9e41af71a439129acc10b24714fc983
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-10-11 11:56:23 +0700

    Fix Swagger lagi

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const cors = require('cors');
 const sequelize = require('./config/db');
 const axios = require('axios');
 const dns = require('dns');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerSetup = require('./config/swagger');
 const userRoutes = require('./routes/userRoutes');
 const adminSiapRoutes = require('./routes/adminSiapRoutes');
 const berkasPenilaianRoutes = require('./routes/berkasPenilaianRoutes');
@@ -27,39 +26,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'SI-MBKM API',
-      version: '1.0.0',
-      description: 'Dokumentasi API untuk Sistem MBKM',
-    },
-    servers: [
-      {
-        url: 'https://backend-si-mbkm.vercel.app',
-      },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
-  },
-  apis: ['./routes/*.js'],
-};
+swaggerSetup(app);
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Tambahkan route untuk root URL
 app.get('/api/hello', (req, res) => {
     res.status(200).json({ message: 'Hello from Vercel API!' });
