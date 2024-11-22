13689e9c35a1607f9b62d34bb55fa1d89014ac4d
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-11-22 17:09:55 +0700

    major change 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,38 +7,28 @@ const Mahasiswa = require('../models/mahasiswa');
 
 // Fungsi Login
 const login = async (req, res) => {
-  const { email, password } = req.body; // Sesuaikan dengan field login
-  const user = await User.findOne({ where: { email } });
+  const { email, password } = req.body;
 
-  if (!user || !(await user.isPasswordValid(password))) {
-    return res.status(401).json({ message: 'Invalid credentials' });
+  try {
+    // Mencari user berdasarkan email
+    const user = await User.findOne({ where: { email } });
+
+    // Memeriksa apakah user ditemukan dan apakah password valid
+    if (!user || !(await user.isPasswordValid(password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Membuat token JWT
+    const token = jwt.sign({ id: user.id, role: user.role, email: user.email }, 'secretKey', { expiresIn: '1d' });
+
+    // Mengirimkan response dengan token
+    res.json({ token });
+  } catch (error) {
+    console.error(error);  // Menambahkan log error untuk debugging
+    res.status(500).json({ message: 'Server error' });
   }
-
-  // Ambil NIP/NIM berdasarkan role pengguna
-  let NIP_or_NIM = null;
-  if (user.role === 'admin_siap') {
-    const admin = await AdminSiap.findOne({ where: { userId: user.id } });
-    NIP_or_NIM = admin.NIP_admin_siap;
-  } else if (user.role === 'dosbing') {
-    const dosbing = await Dosbing.findOne({ where: { userId: user.id } });
-    NIP_or_NIM = dosbing.NIP_dosbing;
-  } else if (user.role === 'koor_mbkm') {
-    const koor = await KoorMbkm.findOne({ where: { userId: user.id } });
-    NIP_or_NIM = koor.NIP_koor_mbkm;
-  } else if (user.role === 'mahasiswa') {
-    const mahasiswa = await Mahasiswa.findOne({ where: { userId: user.id } });
-    NIP_or_NIM = mahasiswa.NIM;
-  }
-
-  // Generate JWT dengan menambahkan email dan NIP/NIM
-  const token = jwt.sign(
-    { id: user.id, role: user.role, email: user.email, NIP_or_NIM },
-    'secretKey',
-    { expiresIn: '1d' }
-  );
-
-  res.json({ token });
 };
+
 
 // Fungsi Register
 const register = async (req, res) => {
