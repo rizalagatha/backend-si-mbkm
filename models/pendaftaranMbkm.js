6b3c0ce8a1191ab29749cfa20d689d52d4d0e497
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-12-03 00:27:26 +0700

    update db

--- models/pendaftaranMbkm.js
+++ models/pendaftaranMbkm.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/db');
 const Mahasiswa = require('./mahasiswa');  // FK ke tabel mahasiswa
 const Dosbing = require('./dosbing');      // FK ke tabel dosbing
 const BerkasPenilaian = require('./berkasPenilaian');
+const ProgramMbkm = require('./programMbkm'); 
 
 const PendaftaranMbkm = sequelize.define('PendaftaranMbkm', {
   id_pendaftaran_mbkm: {
@@ -37,6 +38,18 @@ const PendaftaranMbkm = sequelize.define('PendaftaranMbkm', {
   tanggal: {
     type: DataTypes.DATE,
     allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  id_program_mbkm: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: ProgramMbkm,
+      key: 'id_program_mbkm'
+    }
   }
 }, {
   tableName: 'pendaftaran_mbkm',
