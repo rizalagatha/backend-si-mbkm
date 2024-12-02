69240ad13f26b034c5d5c13631466e9ccb982e67
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-12-02 16:49:46 +0700

    update db

--- models/mahasiswa.js
+++ models/mahasiswa.js
@@ -1,43 +1,51 @@
-module.exports = (sequelize, DataTypes) => {
-  const Mahasiswa = sequelize.define(
-    'Mahasiswa',
-    {
-      NIM: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        allowNull: false,
-      },
-      nama_mahasiswa: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      semester: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      id_program_mbkm: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-      },
-      NIP_dosbing: {
-        type: DataTypes.BIGINT,
-        allowNull: true,
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Mahasiswa = sequelize.define('Mahasiswa', {
+    NIM: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      allowNull: false,
+    },
+    nama_mahasiswa: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    semester: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    id_program_mbkm: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'ProgramMbkm',
+        key: 'id_program_mbkm',
       },
     },
-    {
-      tableName: 'mahasiswa',
-      timestamps: false,
-    }
-  );
+    NIP_dosbing: {
+      type: DataTypes.BIGINT,
+      allowNull: true,
+      references: {
+        model: 'Dosbing',
+        key: 'NIP_dosbing',
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+  }, {
+    tableName: 'mahasiswa',
+    timestamps: false,
+  });
 
-  // Definisikan asosiasi di dalam fungsi associate
   Mahasiswa.associate = (models) => {
     Mahasiswa.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
-    Mahasiswa.hasMany(models.ProgramMbkm, { foreignKey: 'NIM', as: 'programs' });
   };
 
   return Mahasiswa;
--- models/programMbkm.js
+++ models/programMbkm.js
@@ -1,52 +1,57 @@
-module.exports = (sequelize, DataTypes) => {
-  const ProgramMbkm = sequelize.define(
-    'ProgramMbkm',
-    {
-      id_program_mbkm: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      company: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      deskripsi: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      role: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      status: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-      waktu_pelaksanaan: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-      category_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      NIM: {
-        type: DataTypes.BIGINT,
-        allowNull: true,
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const ProgramMbkm = sequelize.define('ProgramMbkm', {
+    id_program_mbkm: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    company: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    deskripsi: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    waktu_pelaksanaan: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    category_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id',
       },
     },
-    {
-      tableName: 'program_mbkm',
-      timestamps: false,
-    }
-  );
+    NIM: {
+      type: DataTypes.BIGINT,
+      allowNull: true,
+      references: {
+        model: 'Mahasiswa',
+        key: 'NIM',
+      },
+    },
+  }, {
+    tableName: 'program_mbkm',
+    timestamps: false,
+  });
 
-  // Definisikan asosiasi di dalam fungsi associate
   ProgramMbkm.associate = (models) => {
     ProgramMbkm.belongsTo(models.Categories, { foreignKey: 'category_id' });
     ProgramMbkm.belongsTo(models.Mahasiswa, { foreignKey: 'NIM', as: 'mahasiswa' });
