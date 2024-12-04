37261f9ed14a688e926030ba7decba13f70164d6
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-12-04 08:58:34 +0700

    update db

--- app.js
+++ app.js
@@ -29,8 +29,19 @@ const User = require('./models/user');
 
 dotenv.config();
 
+const allowedOrigins = ['http://localhost:3000', 'https://your-frontend-domain.com'];
+
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Jika menggunakan cookie
+}));
 app.use(express.json());
 
 swaggerSetup(app);
