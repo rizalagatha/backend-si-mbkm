acb9ea309fdb639b86f21151b35e0f636a792cf6
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2024-12-06 07:19:27 +0700

    update db

--- models/pendaftaranMbkm.js
+++ models/pendaftaranMbkm.js
@@ -1,50 +1,65 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/db');
-const Mahasiswa = require('./mahasiswa');  // FK ke tabel mahasiswa
-const Dosbing = require('./dosbing');      // FK ke tabel dosbing
-const ProgramMbkm = require('./programMbkm'); 
 
-const PendaftaranMbkm = sequelize.define('PendaftaranMbkm', {
-  id_pendaftaran_mbkm: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+class PendaftaranMbkm extends Model {}
+
+PendaftaranMbkm.init(
+  {
+    id_pendaftaran_mbkm: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    NIM: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: 'mahasiswa', // Nama tabel atau model
+        key: 'NIM',
+      },
+    },
+    NIP_dosbing: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: 'dosbing', // Nama tabel atau model
+        key: 'NIP_dosbing',
+      },
+    },
+    tanggal: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id_program_mbkm: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'program_mbkm', // Nama tabel atau model
+        key: 'id_program_mbkm',
+      },
+    },
   },
-  NIM: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    references: {
-      model: Mahasiswa,
-      key: 'NIM'
-    }
-  },
-  NIP_dosbing: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    references: {
-      model: Dosbing,
-      key: 'NIP_dosbing'
-    }
-  },
-  tanggal: {
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-  status: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  id_program_mbkm: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: ProgramMbkm,
-      key: 'id_program_mbkm'
-    }
+  {
+    sequelize,
+    modelName: 'PendaftaranMbkm',
+    tableName: 'pendaftaran_mbkm',
+    timestamps: false,
   }
-}, {
-  tableName: 'pendaftaran_mbkm',
-  timestamps: false
-});
+);
+
+// Define associations
+PendaftaranMbkm.associate = () => {
+  const Mahasiswa = require('./mahasiswa'); // Lazy loading
+  const Dosbing = require('./dosbing'); // Lazy loading
+  const ProgramMbkm = require('./programMbkm'); // Lazy loading
+
+  PendaftaranMbkm.belongsTo(Mahasiswa, { foreignKey: 'NIM', as: 'mahasiswa' });
+  PendaftaranMbkm.belongsTo(Dosbing, { foreignKey: 'NIP_dosbing', as: 'dosbing' });
+  PendaftaranMbkm.belongsTo(ProgramMbkm, { foreignKey: 'id_program_mbkm', as: 'program_mbkm' });
+};
 
 module.exports = PendaftaranMbkm;
