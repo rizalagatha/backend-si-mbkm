1c7d1d1c81f0f312ccca74bbaffca781ae3225a6
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-02-17 15:18:48 +0700

    update db

--- routes/konversiNilaiRoutes.js
+++ routes/konversiNilaiRoutes.js
@@ -70,7 +70,7 @@ const {
  *         description: Konversi nilai berhasil ditambahkan
  */
 
-router.post('/', authenticateToken, authorize(['dosbing', 'mahasiswa', 'koor_mbkm']), createKonversiNilai);
+router.post('/', authenticateToken, authorize(['mahasiswa', 'koor_mbkm']), createKonversiNilai);
 
 /**
  * @swagger
@@ -89,7 +89,7 @@ router.post('/', authenticateToken, authorize(['dosbing', 'mahasiswa', 'koor_mbk
  *                 $ref: '#/components/schemas/KonversiNilai'
  */
 
-router.get('/', authenticateToken, authorize(['admin_siap', 'dosbing', 'mahasiswa', 'koor_mbkm']), getAllKonversiNilai);
+router.get('/', getAllKonversiNilai);
 
 /**
  * @swagger
@@ -113,7 +113,7 @@ router.get('/', authenticateToken, authorize(['admin_siap', 'dosbing', 'mahasisw
  *               $ref: '#/components/schemas/KonversiNilai'
  */
 
-router.get('/:id', authenticateToken, authorize(['admin_siap', 'mahasiswa', 'dosbing', 'koor_mbkm']), getKonversiNilaiById);
+router.get('/:id', getKonversiNilaiById);
 
 /**
  * @swagger
@@ -139,7 +139,7 @@ router.get('/:id', authenticateToken, authorize(['admin_siap', 'mahasiswa', 'dos
  *         description: Data konversi nilai berhasil diperbarui
  */
 
-router.put('/:id', authenticateToken, authorize(['admin_siap', 'mahasiswa', 'dosbing', 'koor_mbkm']), updateKonversiNilai);
+router.put('/:id', authenticateToken, authorize(['mahasiswa', 'koor_mbkm']), updateKonversiNilai);
 
 /**
  * @swagger
@@ -159,6 +159,6 @@ router.put('/:id', authenticateToken, authorize(['admin_siap', 'mahasiswa', 'dos
  *         description: Konversi nilai berhasil dihapus
  */
 
-router.delete('/:id', authenticateToken, authorize(['dosbing', 'mahasiswa', 'koor_mbkm']), deleteKonversiNilai);
+router.delete('/:id', authenticateToken, authorize(['mahasiswa', 'koor_mbkm']), deleteKonversiNilai);
 
 module.exports = router;
